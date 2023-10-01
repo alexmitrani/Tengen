65095f3acf790edf1f79f697bb8034ff8e2c9c75
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2023-10-01 19:43:37 -0300

    Update app.R

--- app.R
+++ app.R
@@ -24,32 +24,55 @@ thematic_shiny(font = "auto")
 
 mydf <- gsheet2tbl('docs.google.com/spreadsheets/d/1WSHIHtk_oKzA5kruweD_3g3OH7DseTsgTLhEFTHsvFQ')
 
+colnames(mydf)[1]="timestamp"
 colnames(mydf)[3]="player.1"
 colnames(mydf)[4]="player.2"
 colnames(mydf)[5]="board_size"
 colnames(mydf)[6]="handicap"
 colnames(mydf)[7]="player_won"
+colnames(mydf)[8]="comment"
 
 mydf <- mydf %>%
-  select(Timestamp, player.1, player.2, board_size, handicap, player_won)
+  select(timestamp, player.1, player.2, board_size, handicap, player_won, comment)
 
 mydf <- mydf %>%
   mutate(player_won = ifelse(player_won=="Negro", 1, 2))
 
 mydf <- mydf %>%
-  pivot_longer(cols = c(player.1, player.2), names_to = "player", values_to = "name")
+  pivot_longer(cols = c(player.1, player.2), names_to = "player", values_to = "player_name")
 
 mydf <- mydf %>%
   mutate(player = ifelse(player=="player.1", 1, 2))
 
 mydf <- mydf %>%
-  mutate(win=ifelse(player==player_won, 1, 0))
+  mutate(player_win=ifelse(player==player_won, 1, 0))
+
+# corrections to player names
 
 mydf <- mydf %>%
-  mutate(loss=1-win)
+  mutate(player_name = ifelse(player_name=="Anibal", "Aníbal", player_name))
+
+# merge on opponent name
+
+mydf2 <- mydf %>%
+  select(timestamp, player, player_name) %>%
+  rename(opponent_name = player_name) %>%
+  mutate(player = if_else(player==1, 2, 1))
 
 mydf <- mydf %>%
-  mutate(name = ifelse(name=="Anibal", "Aníbal", name))
+  left_join(mydf2) %>%
+  mutate(player_colour = ifelse(player==1, "black", "white"))
+
+# prepare final data
+
+mydf <- mydf %>%
+  mutate(comment = ifelse(substr(comment, 1, 4)=="http", paste0("<a href='",  comment, "' target='_blank'>", comment, "</a>"), comment))
+
+mydf <- mydf %>%
+  select(timestamp, board_size, handicap, player_name, player_colour, opponent_name, player_win, comment)
+
+mydf <- mydf %>%
+  arrange(desc(timestamp))
 
 
 # Application -------------------------------------------------------------
@@ -91,11 +114,19 @@ ui <- fluidPage(
 
                               fluidRow(
                                 column(6,
-                                       selectizeInput("playerInput_games", "name:",
-                                                      sort(unique(mydf$name)),
+                                       selectizeInput("player_name_Input_games", "player_name:",
+                                                      sort(unique(mydf$player_name)),
                                                       selected=NULL, multiple =TRUE)),
                                 column(6,
-                                       selectizeInput("board_sizeInput_games", "board_size:",
+                                       selectizeInput("opponent_name_Input_games", "opponent_name:",
+                                                      sort(unique(mydf$opponent_name)),
+                                                      selected=NULL, multiple =TRUE))
+
+                              ),
+
+                              fluidRow(
+                                column(6,
+                                       selectizeInput("board_size_Input_games", "board_size:",
                                                       sort(unique(mydf$board_size)),
                                                       selected=NULL, multiple =TRUE))
 
@@ -145,15 +176,21 @@ server <- function(input, output, session) {
 
     games_data <- mydf
 
-    if (is.null(input$playerInput_games)==FALSE) {
+    if (is.null(input$player_name_Input_games)==FALSE) {
       games_data <- games_data %>%
-        filter(name %in% input$playerInput_games)
+        filter(player_name %in% input$player_name_Input_games)
 
     }
 
-    if (is.null(input$board_sizeInput_games)==FALSE) {
+    if (is.null(input$opponent_name_Input_games)==FALSE) {
       games_data <- games_data %>%
-        filter(board_size %in% input$board_sizeInput_games)
+        filter(opponent_name %in% input$opponent_name_Input_games)
+
+    }
+
+    if (is.null(input$board_size_Input_games)==FALSE) {
+      games_data <- games_data %>%
+        filter(board_size %in% input$board_size_Input_games)
 
     }
 
@@ -164,11 +201,11 @@ server <- function(input, output, session) {
   output$games_datatable <- DT::renderDataTable(DT::datatable({
 
     data <- games_data() %>%
-      arrange(desc(Timestamp))
+      arrange(desc(timestamp))
 
     data
 
-  },
+  }, escape = c(-9),
   style = "bootstrap"))
 
   # end of server -----------------------------------------------------------
