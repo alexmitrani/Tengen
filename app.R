8497bf3254871bde25bc1c10b1b1613903ecbbef
alexmitrani <27974470+alexmitrani@users.noreply.github.com>
2023-10-01 21:11:10 -0300

    Update app.R

--- app.R
+++ app.R
@@ -87,7 +87,7 @@ ui <- fluidPage(
 
   theme = my_theme,
 
-  # tags$head(includeHTML(("google-analytics.html"))),
+  tags$head(includeHTML(("google-analytics.html"))),
 
   tags$style(type = "text/css", "html, body {width:100%; height:100%}"),
 
